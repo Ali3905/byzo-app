7f1a8229fbf4ecaedd1d5f70dca5fedab616243d
Ali3905 <muhammadali30905@gmail.com>
2023-08-13 13:26:10 +0500

    address page

---
+++ src/app/addressbook/page.js
@@ -0,0 +1,14 @@
+import NavBar from '@/components/NavBar'
+import React from 'react'
+import AddressBook from './AddressBook'
+
+const page = () => {
+  return (
+    <div>
+      <NavBar/>
+        <AddressBook />
+    </div>
+  )
+}
+
+export default page
