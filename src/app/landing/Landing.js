06702c2d54c6927e00d93dc0df3afd14c28af109
Ali3905 <muhammadali30905@gmail.com>
2023-08-29 18:59:07 +0500

    heroSection

--- src/app/landing/Landing.js
+++ src/app/landing/Landing.js
@@ -3,11 +3,13 @@ import NavBar from '@/components/NavBar'
 import Footer from '@/components/Footer'
 import BrandsSection from './BrandsSection'
 import MockupSection from './MockupSection'
+import HeroSection from './HeroSection'
 
 const Landing = () => {
   return (
     <>
         <NavBar/>
+          <HeroSection/>
           <BrandsSection />
           <MockupSection />
         <Footer/>
